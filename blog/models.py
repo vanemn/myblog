0e3c70524c5d0b6228b878d88427f81a81b4e409
Vane <vane.moralesn@gmail.com>
2024-11-08 17:02:02 -0300

    ingresa clase al models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title

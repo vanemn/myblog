cd8977e02e46143b1d4a7002232fc4bb0117ec23
Vane <vane.moralesn@gmail.com>
2024-11-08 21:31:51 -0300

    cambios para render

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Apply any outstanding database migrations
-python manage.py migrate
-
 # Convert static asset files
 python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
